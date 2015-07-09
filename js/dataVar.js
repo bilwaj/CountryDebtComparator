5183f0e4d43b78576d2886881bb9ea26a3e30359
bilwaj <bilwaj@gmail.com>
2015-07-08 23:25:02 -0400

    First commit of the country debt comparator
    Inspired by Trading Economics

---
+++ js/dataVar.js
@@ -0,0 +1,182 @@
+var chartData_India = [
+    {
+        "year": 2010,
+        "debtvsgdp": 72.5
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 67.5
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 66.7
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 66.6
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 67.7
+    }
+];
+
+var chartData_China = [
+    {
+        "year": 2010,
+        "debtvsgdp": 17.7
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 33.5
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 28.7
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 26.0
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 22.4
+    }
+];
+
+var chartData_USA = [
+    {
+        "year": 2010,
+        "debtvsgdp": 87.1
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 95.2
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 99.4
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 100.1
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 101.5
+    }
+];
+
+var chartData_Japan = [
+    {
+        "year": 2010,
+        "debtvsgdp": 194.1
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 200.0
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 211.7
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 218.8
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 224.2
+    }
+];
+
+var chartData_Portugal = [
+    {
+        "year": 2010,
+        "debtvsgdp": 83.7
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 94.0
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 111.1
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 125.8
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 129.7
+    }
+];
+
+var chartData_Spain = [
+    {
+        "year": 2010,
+        "debtvsgdp": 54.0
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 60.1
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 69.2
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 84.4
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 92.1
+    }
+];
+var chartData_Greece = [
+    {
+        "year": 2010,
+        "debtvsgdp": 129.7
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 146.0
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 171.3
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 156.9
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 175.0
+    }
+];
+
+var chartData_Italy = [
+    {
+        "year": 2010,
+        "debtvsgdp": 116.4
+    },
+    {
+        "year": 2011,
+        "debtvsgdp": 119.3
+    },
+    {
+        "year": 2012,
+        "debtvsgdp": 116.4
+    },
+    {
+        "year": 2013,
+        "debtvsgdp": 123.1
+    },
+    {
+        "year": 2014,
+        "debtvsgdp": 128.5
+    }
+];
